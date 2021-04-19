70a152d62059445ecd8474d929a2b8935adf18c8
kabbi <userdima@gmail.com>
2021-04-19 19:37:56 +0300

    Remove pcm51xx i2c communications to allow bare pcm5102

--- components/custom_board/pcm51xx/pcm51xx.c
+++ components/custom_board/pcm51xx/pcm51xx.c
@@ -49,18 +49,7 @@ esp_err_t pcm51xx_ctrl(audio_hal_codec_mode_t mode,
                        audio_hal_ctrl_t ctrl_state);
 esp_err_t pcm51xx_config_iface(audio_hal_codec_mode_t mode,
                                audio_hal_codec_i2s_iface_t *iface);
-static i2c_bus_handle_t i2c_handler;
 static int pcm51xx_addr;
-
-/*
- * i2c default configuration
- */
-static i2c_config_t i2c_cfg = {
-    .mode = I2C_MODE_MASTER,
-    .sda_pullup_en = GPIO_PULLUP_ENABLE,
-    .scl_pullup_en = GPIO_PULLUP_ENABLE,
-    .master.clk_speed = 100000,
-};
 
 /*
  * Operate function
@@ -79,126 +68,27 @@ audio_hal_func_t AUDIO_CODEC_PCM51XX_DEFAULT_HANDLE = {
 
 static esp_err_t pcm51xx_transmit_registers(const pcm51xx_cfg_reg_t *conf_buf,
                                             int size) {
-  int i = 0;
-  esp_err_t ret = ESP_OK;
-  while (i < size) {
-    ret = i2c_bus_write_bytes(i2c_handler, pcm51xx_addr,
-                              (unsigned char *)(&conf_buf[i].offset), 1,
-                              (unsigned char *)(&conf_buf[i].value), 1);
-    i++;
-  }
-  if (ret != ESP_OK) {
-    ESP_LOGE(TAG, "Fail to load configuration to pcm51xx");
-    return ESP_FAIL;
-  }
-  ESP_LOGI(TAG, "%s:  write %d reg done", __FUNCTION__, i);
-  return ret;
+  return ESP_OK;
 }
 
 esp_err_t pcm51xx_init(audio_hal_codec_config_t *codec_cfg) {
-  esp_err_t ret = ESP_OK;
-  ESP_LOGI(TAG, "Power ON CODEC with GPIO %d", PCM51XX_RST_GPIO);
-  // probably unnecessary...
-  /*
-gpio_config_t io_conf;
-io_conf.pin_bit_mask = BIT64(PCM51XX_RST_GPIO);
-io_conf.mode = GPIO_MODE_OUTPUT;
-io_conf.intr_type = GPIO_INTR_DISABLE;
-gpio_config(&io_conf);
-gpio_set_level(PCM51XX_RST_GPIO, 0);
-vTaskDelay(20 / portTICK_RATE_MS);
-gpio_set_level(PCM51XX_RST_GPIO, 1);
-vTaskDelay(200 / portTICK_RATE_MS);
-  */
-
-  ret = get_i2c_pins(I2C_NUM_0, &i2c_cfg);
-  i2c_handler = i2c_bus_create(I2C_NUM_0, &i2c_cfg);
-  if (i2c_handler == NULL) {
-    ESP_LOGW(TAG, "failed to create i2c bus handler\n");
-    return ESP_FAIL;
-  }
-
-  uint8_t data[] = {0, 0};
-  for (int i = 0; i < 4; i++) {
-    pcm51xx_addr = PCM51XX_BASE_ADDR + 2 * i;
-    ESP_LOGI(TAG, "Looking for a pcm51xx chip at address 0x%x", pcm51xx_addr);
-    ret = i2c_bus_write_data(i2c_handler, pcm51xx_addr, data, 0);
-    if (ret == ESP_OK) {
-      ESP_LOGI(TAG, "Found a pcm51xx chip at address 0x%x", pcm51xx_addr);
-      break;
-    }
-  }
-
-  PCM51XX_ASSERT(ret, "Fail to detect pcm51xx PA", ESP_FAIL);
-  ret |= pcm51xx_transmit_registers(
-      pcm51xx_init_seq, sizeof(pcm51xx_init_seq) / sizeof(pcm51xx_init_seq[0]));
-
-  PCM51XX_ASSERT(ret, "Fail to iniitialize pcm51xx PA", ESP_FAIL);
-  return ret;
+  return ESP_OK;
 }
 
 esp_err_t pcm51xx_set_volume(int vol) {
-  // vol is given as 1/2dB step with
-  // 255: -inf (mute)
-  // 254: -103dB
-  // 48:  0dB
-  // 0 (max): +24dB
-
-  if (vol < PCM51XX_VOLUME_MIN) {
-    vol = PCM51XX_VOLUME_MIN;
-  }
-  if (vol > PCM51XX_VOLUME_MAX) {
-    vol = PCM51XX_VOLUME_MAX;
-  }
-  uint8_t cmd[2] = {0, 0};
-  esp_err_t ret = ESP_OK;
-
-  cmd[1] = vol;
-
-  cmd[0] = PCM51XX_REG_VOL_L;
-  ret = i2c_bus_write_bytes(i2c_handler, pcm51xx_addr, &cmd[0], 1, &cmd[1], 1);
-  cmd[0] = PCM51XX_REG_VOL_R;
-  ret |= i2c_bus_write_bytes(i2c_handler, pcm51xx_addr, &cmd[0], 1, &cmd[1], 1);
-  ESP_LOGW(TAG, "Volume set to 0x%x", cmd[1]);
-  return ret;
+  return ESP_OK;
 }
 
 esp_err_t pcm51xx_get_volume(int *value) {
-  /// FIXME: Got the digit volume is not right.
-  uint8_t cmd[2] = {PCM51XX_REG_VOL_L, 0x00};
-  esp_err_t ret =
-      i2c_bus_read_bytes(i2c_handler, pcm51xx_addr, &cmd[0], 1, &cmd[1], 1);
-  PCM51XX_ASSERT(ret, "Fail to get volume", ESP_FAIL);
-  ESP_LOGI(TAG, "Volume is %d", cmd[1]);
-  *value = cmd[1];
-  return ret;
+  return ESP_OK;
 }
 
 esp_err_t pcm51xx_set_mute(bool enable) {
-  esp_err_t ret = ESP_OK;
-  uint8_t cmd[2] = {PCM51XX_REG_MUTE, 0x00};
-  ret |= i2c_bus_read_bytes(i2c_handler, pcm51xx_addr, &cmd[0], 1, &cmd[1], 1);
-
-  if (enable) {
-    cmd[1] |= 0x11;
-  } else {
-    cmd[1] &= (~0x11);
-  }
-  ret |= i2c_bus_write_bytes(i2c_handler, pcm51xx_addr, &cmd[0], 1, &cmd[1], 1);
-
-  PCM51XX_ASSERT(ret, "Fail to set mute", ESP_FAIL);
-  return ret;
+  return ESP_OK;
 }
 
 esp_err_t pcm51xx_get_mute(bool *enabled) {
-  esp_err_t ret = ESP_OK;
-  uint8_t cmd[2] = {PCM51XX_REG_MUTE, 0x00};
-  ret |= i2c_bus_read_bytes(i2c_handler, pcm51xx_addr, &cmd[0], 1, &cmd[1], 1);
-
-  PCM51XX_ASSERT(ret, "Fail to get mute", ESP_FAIL);
-  *enabled = (bool)(cmd[1] & 0x11);
-  ESP_LOGI(TAG, "Get mute value: %s", *enabled ? "muted" : "unmuted");
-  return ret;
+  return ESP_OK;
 }
 
 esp_err_t pcm51xx_deinit(void) {
